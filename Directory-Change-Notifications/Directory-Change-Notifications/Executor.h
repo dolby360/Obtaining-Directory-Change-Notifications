242c8586d85238f20324ec3b56e694efd9f031aa
dolev <mrdolbyking@gmail.com>
2017-10-08 21:11:30 +0300

    Watcher as a thread, there are still bugs in the system.

---
+++ Directory-Change-Notifications/Directory-Change-Notifications/Executor.h
@@ -0,0 +1,39 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+#include "Watcher.h"
+
+public ref class Executor{
+private:
+	static Executor^ inst;
+	Watcher^ watch;
+	Executor(){
+		
+	}
+
+public:
+	void setWtcher(Watcher^ newWatch){ watch = newWatch; }
+	static Executor^ getInstance(){
+		if (inst != nullptr){
+			return inst;
+		}
+		inst = gcnew Executor();
+		return inst;
+	}
+
+	void stopTheThread(){
+		if (watch == nullptr){
+			return;
+		}
+		watch->stopWatching();
+	}
+
+	[PermissionSet(SecurityAction::Demand, Name = "FullTrust")]
+	void static execute(){
+		Watcher^ w = gcnew Watcher("C:\\Users\\dolev\\Desktop\\fol1\\file1.txt");
+		Executor::getInstance()->setWtcher(w);
+		w->run();
+	}
+};
+
+#endif
--- Directory-Change-Notifications/Directory-Change-Notifications/main.cpp
+++ Directory-Change-Notifications/Directory-Change-Notifications/main.cpp
@@ -1,9 +1,17 @@
 
+#include <windows.h>
+#include <stdlib.h>
+#include <tchar.h>
 #include <stdio.h>
 #include <conio.h>
 
-
+#include "Watcher.h"
+#include "Executor.h"
 void main(){
+	
+	System::Threading::Thread^ oThread = gcnew System::Threading::Thread(
+		gcnew System::Threading::ThreadStart(&Executor::execute));
+
 	printf("Initial commit\n");
 	getch();
 }
